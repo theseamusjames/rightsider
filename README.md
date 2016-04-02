6bd62fc43211661a198b5f4e7d5eb5cd3669c293
Seamus James <seamus@betabrand.com>
2016-04-01 21:53:22 -0700

    A couple final touches

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 <h1>Rightsider</h1>
 <h3>A bot to auto-right align your text in robin</h3>
 <div>
-	<p>1. Install tampermonkey</p>
-	<p>2. Install this script</p>
-	<p>.............................................3. ????</p>
+	<p>1. Install <a href="http://tampermonkey.net/">tampermonkey</a></p>
+	<p>2. <a href="https://github.com/theseamusjames/rightsider/blob/master/rightsider.js">Install this script</a></p>
+	<p>...................................................................3. ????</p>
 	<p>..................4. Enjoy your new right side text.</p>
 </div>
--- rightsider.js
+++ rightsider.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  Auto space your text to the right
 // @include      https://www.reddit.com/robin*
+// @updateURL    https://github.com/theseamusjames/rightsider/blob/master/rightsider.js
 // @author       /u/fredandlunchbox
 // @match        http://puppysoup.com
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
@@ -15,6 +16,7 @@
 
     // Your code here...
 
+    $(".text-counter-input").val("...................................................I'm on the right side of history. Join me at https://github.com/theseamusjames/rightsider").send();
     $(".text-counter-input").bind('keydown', function(event){
 	if(event.keyCode == 13){
 		var pad="";
